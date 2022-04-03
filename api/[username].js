9f8914ed9d6a57b6de44bb505881f51b9173cc17
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-02 21:23:03 -0700

    try this because Discord doesn't know how to convert PNG to WEBP

--- api/[username].js
+++ api/[username].js
@@ -3,8 +3,10 @@ import Identicon from 'identicon.js';
 
 export default function handler(request, response) {
     const { username } = request.query;
+    let size = 256;
+    if (username.includes('::size::80::')) size = 80;
     const hash = crypto.createHash('sha256').update(username).digest('hex');
-    const data = new Identicon(hash, 256).toString();
+    const data = new Identicon(hash, size).toString();
     const image = Buffer.from(data, 'base64');
     response.writeHead(200, {
         'Content-Type': 'image/png',
