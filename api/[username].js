96bf63e31129eea795f854109c52d8719f88adbb
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-02 21:16:49 -0700

    try changing size because Discord is bad

--- api/[username].js
+++ api/[username].js
@@ -4,7 +4,7 @@ import Identicon from 'identicon.js';
 export default function handler(request, response) {
     const { username } = request.query;
     const hash = crypto.createHash('sha256').update(username).digest('hex');
-    const data = new Identicon(hash, 512).toString();
+    const data = new Identicon(hash, 256).toString();
     const image = Buffer.from(data, 'base64');
     response.writeHead(200, {
         'Content-Type': 'image/png',
